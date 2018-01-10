865079e5eb97f0b29b44e239baac515c62c3647a
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-01-10 15:27:47 -0800

    made by me

--- spec/jasmine_examples/quadraticTest.js
+++ spec/jasmine_examples/quadraticTest.js
@@ -23,29 +23,6 @@ describe("quadratic", () => {
                 expect(quadratic('a', 2, 6)).toBe('NaN and NaN')
             })
         })
-//
-//        describe('positive', () => {
-//            it("should expect '(1, 2, 3)' to give '3'", () => {
-//                expect('quadratic.positive (1, 2, 3)').toBe('3')
-//            })
-//            it('sholud throw errors', () => {
-//                expect('quadractic.positive (a, 2, 6)').throwError('impossible error')
-//                expect('quadractic.positive (5, b, 3)').throwError('impossible error')
-//                expect('quadractic.positive (1, 4, c)').throwError('impossible error')
-//                expect('quadractic.positive (a, b, c)').throwError('impossible error')
-//            })
-//        })
-//
-//        describe('ngative', () => {
-//            it("should expect '(1, 2, 3)' to give '3'", () => {
-//                expect('quadratic.negative (1, 2, 3)').toBe('1')
-//            })
-//            it('should throw errors', () => {
-//                expect('quadractic.nagative (a, 2, 6)').throwError('impossible error')
-//                expect('quadractic.negative (5, b, 3)').throwError('impossible error')
-//                expect('quadractic.negative (1, 4, c)').throwError('impossible error')
-//                expect('quadractic.negative (a, b, c)').throwError('impossible error')
-//            })
-//        })
+
     })
 })
